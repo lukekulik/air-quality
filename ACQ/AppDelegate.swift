b05fbc7c6a0816d58001e2407cc9d4d82b9b379b
Luke K <lkulik@jarvis.local>
2015-11-27 14:37:34 +0800

    Initial Commit

---
+++ ACQ/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ACQ
+//
+//  Created by Luke K. on 27/11/15.
+//  Copyright © 2015 Luke Kulik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ACQ/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ACQ
+//
+//  Created by Luke K. on 27/11/15.
+//  Copyright © 2015 Luke Kulik. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
